f08e2a789683029042b998d3f5cf1d53db304206
AnishBacchus <anishbacchus@gmail.com>
2026-03-26 12:09:43 -0400

    Added a User entity with private fields.

--- GameNight/GameNight/src/main/java/entities/User.java
+++ GameNight/GameNight/src/main/java/entities/User.java
@@ -1,8 +1,16 @@
 package entities;
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class User {
+
+    
+    private long id;
+
     private String name;
     private String email;
     private String password;
