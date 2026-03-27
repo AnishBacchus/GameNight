27c6b88c4fefca7758052d406533eef484c80314
AnishBacchus <anishbacchus@gmail.com>
2026-03-27 14:53:05 -0400

    Created and updated User entity.

--- GameNight/GameNight/src/main/java/entities/User.java
+++ GameNight/GameNight/src/main/java/entities/User.java
@@ -1,5 +1,8 @@
 package entities;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +11,8 @@ import lombok.Setter;
 @Setter
 public class User {
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String name;
