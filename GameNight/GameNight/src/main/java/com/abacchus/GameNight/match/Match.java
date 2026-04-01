a759244f69f192c35d7dc0c3cf33c8fb50687a52
AnishBacchus <anishbacchus@gmail.com>
2026-03-31 21:33:02 -0400

    Created Match entity with fields and relations.

---
+++ GameNight/GameNight/src/main/java/com/abacchus/GameNight/match/Match.java
@@ -0,0 +1,36 @@
+package com.abacchus.GameNight.match;
+
+import com.abacchus.GameNight.game.Game;
+import com.abacchus.GameNight.gamegroup.GameGroup;
+import com.abacchus.GameNight.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "match")
+public class Match {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "match_creator_id")
+    private User matchCreator;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private GameGroup gameGroup;
+
+    @ManyToOne
+    @JoinColumn(name = "game_played")
+    private Game game;
+
+    private LocalDateTime playedAt;
+    private boolean isDisputed;
+}
