0fcff9698407c43dd902a8d8e33d76347b468be8
AnishBacchus <anishbacchus@gmail.com>
2026-03-31 21:14:48 -0400

    Updated Game entity.

--- GameNight/GameNight/src/main/java/com/abacchus/GameNight/game/Game.java
+++ GameNight/GameNight/src/main/java/com/abacchus/GameNight/game/Game.java
@@ -1,12 +1,20 @@
 package com.abacchus.GameNight.game;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "game")
 public class Game {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String category;
+    private String description;
 }
