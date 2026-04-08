7b44007d0318b38871bcc5e676e6dd774aa1683e
AnishBacchus <anishbacchus@gmail.com>
2026-04-08 13:00:32 -0400

    Updated MatchResult repository.

--- GameNight/GameNight/src/main/java/com/abacchus/GameNight/matches/MatchResultRepository.java
+++ GameNight/GameNight/src/main/java/com/abacchus/GameNight/matches/MatchResultRepository.java
@@ -1,7 +1,11 @@
 package com.abacchus.GameNight.matches;
 
+import com.abacchus.GameNight.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MatchResultRepository extends JpaRepository <MatchResult, Long> {
+import java.util.List;
 
+public interface MatchResultRepository extends JpaRepository <MatchResult, Long> {
+    List<MatchResult> findByUserId(long userId);
+    MatchResult findByUserIdAndMatchId(long userId, long matchId);
 }
