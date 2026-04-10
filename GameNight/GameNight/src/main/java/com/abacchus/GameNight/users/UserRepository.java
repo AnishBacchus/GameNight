1050fac122bd6fefcc56bd8398ab7d805441d153
AnishBacchus <anishbacchus@gmail.com>
2026-04-10 15:12:35 -0400

    Updated UserRepository with new methods to find by username and email.

--- GameNight/GameNight/src/main/java/com/abacchus/GameNight/users/UserRepository.java
+++ GameNight/GameNight/src/main/java/com/abacchus/GameNight/users/UserRepository.java
@@ -2,6 +2,9 @@ package com.abacchus.GameNight.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUserId(Long id);
+    Optional<User> findByUsername(Long id);
+    Optional<User> findByEmail(Long id);
 }
