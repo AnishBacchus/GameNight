128fbd5a2824f8d8d5641bfe0529342e8913833b
AnishBacchus <anishbacchus@gmail.com>
2026-04-12 22:44:31 -0400

    Created UserService class with method to create a user.

---
+++ GameNight/GameNight/src/main/java/com/abacchus/GameNight/users/UserService.java
@@ -0,0 +1,24 @@
+package com.abacchus.GameNight.users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public ResponseEntity<String> createUser(UserCreateDTO userCreateDTO){
+        User user = new User();
+
+        user.setUsername(userCreateDTO.getUsername());
+        user.setPassword(userCreateDTO.getPassword());
+        user.setEmail(userCreateDTO.getEmail());
+
+        userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body("User account created!");
+    }
+}
