1bf045d5c0376af43ba0f35af164295c7fab35a3
AnishBacchus <anishbacchus@gmail.com>
2026-04-18 20:36:15 -0400

    Created the get method in the UserController to getUserById.

--- GameNight/GameNight/src/main/java/com/abacchus/GameNight/users/UserController.java
+++ GameNight/GameNight/src/main/java/com/abacchus/GameNight/users/UserController.java
@@ -4,10 +4,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -22,5 +19,10 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
+    @GetMapping("/getUser/{id}")
+    public ResponseEntity<UserResponseDTO> getUserById(@PathVariable Long id){
+        UserResponseDTO user = userService.getUser(id);
+        return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
 
 }
