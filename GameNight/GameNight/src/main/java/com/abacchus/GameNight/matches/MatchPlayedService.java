ab3e42c713200cb47b0b87060ff456b514a40bf3
AnishBacchus <anishbacchus@gmail.com>
2026-04-27 11:45:48 -0400

    Created MatchPlayedService

---
+++ GameNight/GameNight/src/main/java/com/abacchus/GameNight/matches/MatchPlayedService.java
@@ -0,0 +1,16 @@
+package com.abacchus.GameNight.matches;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MatchPlayedService {
+
+    @Autowired
+    MatchPlayedRepository matchPlayedRepository;
+
+    public MatchPlayedRepository createMatch() {
+
+        }
+    }
+
